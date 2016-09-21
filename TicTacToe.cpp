a4c3d1496eda71d96bea1b142d299da7d108a65a
U-BSD\293359 <293359@SHS-10D8A831B.beaverton.k12.or.us>
2016-09-21 14:07:44 -0700

    Initiliazed cpp file and added grid and board arrays

---
+++ TicTacToe.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int grid[3][3];
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      grid[i][j] = 0;
+    }
+  }
+  char board[7][4];
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      grid[i][j] = 0;
+    }
+  }
+  
+  
+  
+  
+  
+  
+}
+
+
+/*   1 2 3
+ * a X X O
+ * b O X X
+ * c O O X
+ * 
+ */
